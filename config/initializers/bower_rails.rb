a8bea7d3e45120313572c7405082d84b31bde09c
Cristiano Betta <cbetta@gmail.com>
2014-08-17 20:45:31 +0100

    Using google fonts and bootstrap for styling

---
+++ config/initializers/bower_rails.rb
@@ -0,0 +1,13 @@
+BowerRails.configure do |bower_rails|
+  # Tell bower-rails what path should be considered as root. Defaults to Dir.pwd
+  # bower_rails.root_path = Dir.pwd
+
+  # Invokes rake bower:install before precompilation. Defaults to false
+  # bower_rails.install_before_precompile = true
+
+  # Invokes rake bower:resolve before precompilation. Defaults to false
+  # bower_rails.resolve_before_precompile = true
+
+  # Invokes rake bower:clean before precompilation. Defaults to false
+  # bower_rails.clean_before_precompile = true
+end
