1ec05af3fbaf65e7b1c8a7ea622873cf27da75ef
Fares Alaboud <fareskalaboud@users.noreply.github.com>
2016-12-17 14:16:59 +0300

    Added text-to-speech

---
+++ app/assets/javascripts/accessibility.js
@@ -0,0 +1,37 @@
+$(document).on("click", "#quick-version-speech", function() {
+  responsiveVoice.speak("Our hackathon is dedicated to providing a harassment-free experience for everyone, regardless of the following.");
+  responsiveVoice.speak("gender");
+  responsiveVoice.speak("gender identity and expression");
+  responsiveVoice.speak("age");
+  responsiveVoice.speak("sexual orientation");
+  responsiveVoice.speak("disability");
+  responsiveVoice.speak("physical appearance");
+  responsiveVoice.speak("body size");
+  responsiveVoice.speak("race");
+  responsiveVoice.speak("ethnicity");
+  responsiveVoice.speak("nationality");
+  responsiveVoice.speak("religion");
+  responsiveVoice.speak("previous hackathon attendance, or lack of");
+  responsiveVoice.speak("computing experience, or lack of");
+  responsiveVoice.speak("chosen programming language or tech stack");
+  responsiveVoice.speak("We do not tolerate harassment of hackathon participants in any form. Sexual language and imagery is not appropriate at any hackathon venue, this includes the following.");
+  responsiveVoice.speak("hacks");
+  responsiveVoice.speak("talks, presentations, or demos");
+  responsiveVoice.speak("workshops");
+  responsiveVoice.speak("any parties associated to the hackathon");
+  responsiveVoice.speak("social media");
+  responsiveVoice.speak("any other online media");
+  responsiveVoice.speak("Hackathon participants violating these rules may be sanctioned or expelled from the hackathon without a refund (if applicable) at the discretion of the hackathon organisers.");
+});
+
+$(document).on("click", "#less-quick-version-speech", function() {
+  responsiveVoice.speak("Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, nationality, or religion, sexual images in public spaces, deliberate intimidation, stalking, following, photography or audio/video recording against reasonable consent, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.")
+  responsiveVoice.speak("Photography is encouraged, but other participants must be given a reasonable chance to opt out from being photographed. If they object to the taking of their photograph, comply with their request. It is inappropriate to take photographs in contexts where people have a reasonable expectation of privacy (in bathrooms or where participants are sleeping).")
+  responsiveVoice.speak("Participants asked to stop any harassing behavior are expected to comply immediately.")
+  responsiveVoice.speak("As this is a hackathon, we like to explicitly note that the hacks created at our hackathon are equally subject to the anti-harassment policy.")
+  responsiveVoice.speak("Sponsors and partners are also subject to the anti-harassment policy. In particular, sponsors should not use sexualised images, activities, or other material. Sponsor representatives (including volunteers) should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised environment.")
+  responsiveVoice.speak("If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of hackathon staff immediately.")
+  responsiveVoice.speak("Hackathon staff will be happy to help participants contact any local security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the hackathon. We value your attendance.")
+  responsiveVoice.speak("If a participant engages in harassing behavior, the hackathon organisers may take any action they deem appropriate. This includes warning the offender, expulsion from the hackathon with no refund (if applicable), or reporting their behaviour to local law enforcement.")
+  responsiveVoice.speak("We expect participants to follow these rules at hackathon and workshop venues and hackathon-related social events.")
+});
